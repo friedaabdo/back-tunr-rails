c20750bb0c024abb569017be626c23a058a284f7
Frieda Abdo <friedabdo@gmail.com>
2020-11-16 11:53:03 -0500

    seed, migrate

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Song.create([
+    {title: 'positions', artist: 'Ariana Grande', time: '2:53'},
+    {title: 'You Want It Darker', artist: 'Leonard Cohen', time: '4:44'},
+    {title: 'Black Velvet', artist: 'Alannah Myles', time: '4:47'},
+    {title: 'I Want It That Way', artist: 'Backstreet Boys', time: '3:34'},
+    {title: 'Homicide', artist: 'Logic, Eminem', time: '4:05'},
+    {title: 'From Eden', artist: 'Hozier', time: '4:42'}
+])
